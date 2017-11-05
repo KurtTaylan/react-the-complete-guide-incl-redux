9129fbdddf1c62e381401692934df531d8a87956
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 09:59:39 +0300

    Passing Method References Between Parent-Child Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,18 +14,25 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hi, I am Taylan</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <button onClick={() => this.switchNameHandler('Inefficient, Taylan button click')}>Switch Name</button>
+
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Efficient, Taylan paragraph click')}>My Hobbies: Racing </Person>
             </div>
         );
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         console.log('Was clicked!');
         this.setState({
             persons: [
-                {name: 'Taylanos', age: 24},
+                {name: newName, age: 24},
                 {name: 'Yesim', age: 23}
             ]
         });
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const person = (props) => {
     return (
         <div>
-            <p>I'm a {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
     );

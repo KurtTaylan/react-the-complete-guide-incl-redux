961ab03d9a4e3fe0808a70539676ffbed354890e
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 10:22:14 +0300

    Adding style to components

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>

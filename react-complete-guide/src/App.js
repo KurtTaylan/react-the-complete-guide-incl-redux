0d30358ed52958b627a40988c79c3fcb05878c1b
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 08:47:45 +0300

    Functional(Stateless) Person component is created.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I am Taylan</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I am Taylan</h1>
+                <Person/>
+                <Person/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const person = () => {
+    return <p>I'm a Person!</p>
+};
+
+export default person;

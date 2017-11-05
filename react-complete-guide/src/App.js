b989c62883333c0da44b0d0b6994b3a3021c8d92
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 11:58:32 +0300

    Mapping over array to show list items dynamicly

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
     state = {
         persons: [
-            {name: 'Taylan', age: 24},
-            {name: 'Yesim', age: 23}
+            {id: '1', name: 'Taylan', age: 24},
+            {id: '2', name: 'Yesim', age: 23}
         ],
         showPersons: false
     };
@@ -24,16 +25,13 @@ class App extends Component {
         if (this.state.showPersons) {
             conditionalPersons = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}/>
-
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'Efficient, Taylan paragraph click')}
-                        changed={this.nameChangedHandler}>My Hobbies:
-                        Racing </Person>
+                    {this.state.persons.map((person, index) => {
+                        return <Person
+                            key={person.id}
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}/>
+                    })}
                 </div>
             );
         }
@@ -49,21 +47,16 @@ class App extends Component {
         );
     }
 
-
     togglePersonsHandler = () => {
         console.log('Was clicked!');
         const doesShow = this.state.showPersons;
         this.setState({showPersons: !doesShow});
     };
 
-    switchNameHandler = (newName) => {
-        console.log('Was clicked!');
-        this.setState({
-            persons: [
-                {name: newName, age: 24},
-                {name: 'Yesim', age: 23}
-            ]
-        });
+    deletePersonHandler = (personIndex) => {
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
     };
 
     nameChangedHandler = (event) => {

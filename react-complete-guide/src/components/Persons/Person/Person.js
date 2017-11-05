98ecb62a9c42d78e848480388ee96aa486cae8ce
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 20:41:27 +0300

    Convert to Statefull Component from Stateless Component

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {Component} from 'react';
 import styledClasses from './Person.css'
 
-const person = (props) => {
-    return (
-        <div className={styledClasses.Person}>
-            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    );
-};
 
-export default person;
+class Person extends Component {
+    render() {
+        return (
+            <div className={styledClasses.Person}>
+                <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
+}
+
+export default Person;
--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <ErrorBoundary key={person.id}>
-        <Person
-            click={() => props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            changed={(event) => props.changed(event, person.id)}/>
-    </ErrorBoundary>
-});
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id}>
+                <Person
+                    click={() => this.props.clicked(index)}
+                    name={person.name}
+                    age={person.age}
+                    changed={(event) => this.props.changed(event, person.id)}/>
+            </ErrorBoundary>
+        });
+    }
+}
 
-export default persons;
+export default Persons;

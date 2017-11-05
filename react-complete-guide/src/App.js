462e138258bebc864f6541808d7bcb75084d006e
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 10:27:20 +0300

    Adding inline style to components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,10 +11,21 @@ class App extends Component {
     };
 
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
+
         return (
             <div className="App">
                 <h1>Hi, I am Taylan</h1>
-                <button onClick={() => this.switchNameHandler('Inefficient, Taylan button click')}>Switch Name</button>
+                <button style={style} onClick={() => this.switchNameHandler('Inefficient, Taylan button click')}>Switch
+                    Name
+                </button>
 
                 <Person
                     name={this.state.persons[0].name}

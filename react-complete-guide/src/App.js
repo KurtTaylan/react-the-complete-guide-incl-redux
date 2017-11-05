0b38b95ba8e36edabb666e5406d7fc0b6782086c
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 09:21:42 +0300

    Props & States: Pass down the data and change component state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Taylan', age: 24},
+            {name: 'Yesim', age: 23}
+        ]
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I am Taylan</h1>
-                <Person/>
-                <Person/>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
             </div>
         );
     }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const person = () => {
-    return <p>I'm a Person!</p>
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    );
 };
 
 export default person;

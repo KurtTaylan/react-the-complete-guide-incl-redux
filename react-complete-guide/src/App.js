46b2294093940fc6c61ad9e9e8ab1889232b9a0a
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-05 09:38:07 +0300

    Re-render DOM by changing State & Props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,12 +14,23 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hi, I am Taylan</h1>
-                <button>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
             </div>
         );
     }
+
+    switchNameHandler = () => {
+        console.log('Was clicked!');
+        this.setState({
+            persons: [
+                {name: 'Taylanos', age: 24},
+                {name: 'Yesim', age: 23}
+            ]
+        });
+    };
+
 }
 
 export default App;

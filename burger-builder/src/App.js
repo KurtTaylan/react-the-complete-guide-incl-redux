e5fe7e1eb63dd525547a6b920deefaa734ec7ad2
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-08 14:54:15 +0300

    Burger-builder: Creating Layout Component as roof of application

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import Layout from "./components/Layout/Layout";
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello World!
+                <Layout>
+                    Hello World!
+                </Layout>
             </div>
         );
     }

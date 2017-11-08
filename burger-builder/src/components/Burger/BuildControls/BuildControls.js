04ce03be876e3418f73881e63209b50983d6747d
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-08 21:47:38 +0300

    Burger-builder: Displaying and Updating Burger Price

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -11,6 +11,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={styleClasses.BuildControls}>
+        <p>Total Price: <strong>{props.totalPrice.toFixed(2)}</strong></p>
         {controls.map(control => (
             <BuildControl
                 key={control.label}
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    totalPrice={this.state.totalPrice}
                 />
             </Aux>
         );

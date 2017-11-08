c04f5037c13b56d7ba035dad0908b2b2c2ced98a
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-08 21:02:08 +0300

    Burger-builder: Outputting multiple build controls

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styleClasses from './BuildControls.css';
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
-    <div></div>
+    <div className={styleClasses.BuildControls}>
+        {controls.map(cntrl => (
+            <BuildControl key={cntrl.label} ingredientLabel={cntrl.label}/>
+        ))}
+    </div>
 );
 
 export default buildControls;
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
 
@@ -17,7 +18,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Control</div>
+                <BuildControls/>
             </Aux>
         );
     }

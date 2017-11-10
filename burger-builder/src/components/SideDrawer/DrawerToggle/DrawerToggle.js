e847ecf1738222c1559eabba3b92201c14098890
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-10 21:03:51 +0300

    Burger-builder: Adding hamburger menu Icon

--- burger-builder/src/components/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burger-builder/src/components/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import styledClasses from './DrawerToggle.css';
 
 const drawerToggle = (props) => {
     return (
-        <div onClick={props.toggled}>MENU</div>
+        <div className={styledClasses.DrawerToggle}
+             onClick={props.toggled}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     );
 };
 

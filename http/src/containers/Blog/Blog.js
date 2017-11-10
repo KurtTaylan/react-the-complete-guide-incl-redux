f0b9b043083adcd146ef79f6395901c9069747f8
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 02:13:27 +0300

    Http: Creating HTTP request to GET data

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    componentDidMount() {
+        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response);
+            });
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    <Post/>
+                    <Post/>
+                    <Post/>
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );

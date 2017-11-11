e5b789e6528c0596608424f445632474ceac25d4
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 10:25:06 +0300

    Http: Rendering fetched data to screen

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -6,21 +6,26 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    };
 
     componentDidMount() {
         const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                this.setState({posts: response.data});
+                //console.log(response);
             });
     }
 
     render() {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title}/>;
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post/>
-                    <Post/>
-                    <Post/>
+                    {posts}
                 </section>
                 <section>
                     <FullPost/>

3b8dcab58e3893a4b710ec5024b65d217ca916a1
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 11:24:09 +0300

    Http: Post data to the Server

--- http/src/components/NewPost/NewPost.js
+++ http/src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from "axios";
 
 import './NewPost.css';
 
@@ -7,22 +8,37 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
-    }
+    };
 
-    render () {
+    postDataHandler = () => {
+        const post = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author
+        };
+
+        axios.post('https://jsonplaceholder.typicode.com/posts', post)
+            .then(response => {
+                console.log(response);
+            });
+    };
+
+    render() {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title}
+                       onChange={(event) => this.setState({title: event.target.value})}/>
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content}
+                          onChange={(event) => this.setState({content: event.target.value})}/>
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }

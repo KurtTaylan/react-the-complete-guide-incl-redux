60ce21e32dd2b32b6ad038ae283ac40cf57b66cb
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 23:48:24 +0300

    Routing: Dynamic Nested Routes.

--- routing/src/containers/Blog/FullPost/FullPost.js
+++ routing/src/containers/Blog/FullPost/FullPost.js
@@ -10,8 +10,16 @@ class FullPost extends Component {
 
     componentDidMount() {
         console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.loadData();
+    }
+
+    loadData() {
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
                 axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         // console.log(response);
@@ -21,8 +29,9 @@ class FullPost extends Component {
         }
     }
 
+
     deletePostHandler = () => {
-        axios.delete(this.props.location.pathname + this.props.id)
+        axios.delete(this.props.location.pathname + this.props.match.params.id)
             .then(response => {
                 console.log(response);
             });
@@ -30,7 +39,7 @@ class FullPost extends Component {
 
     render() {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{textAlign: 'center'}}>Loading...!</p>;
         }
         if (this.state.loadedPost) {

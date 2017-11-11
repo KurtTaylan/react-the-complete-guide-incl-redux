0f74cac751affe473d6029c127af08841b7602dc
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 10:32:59 +0300

    Http: transforming data before show in componentDidMount

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -13,14 +13,22 @@ class Blog extends Component {
     componentDidMount() {
         const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Taylan'
+                    }
+                });
+
+                this.setState({posts: updatedPosts});
                 //console.log(response);
             });
     }
 
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title}/>;
+            return <Post title={post.title} author={post.author}/>;
         });
         return (
             <div>

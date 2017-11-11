3a6ad34bba407b5f1128ad9bbf3333eca5004189
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 14:14:59 +0300

    Http: Adding interceptors

--- http/src/index.js
+++ http/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(request => {
+        console.log(request);
+        // edit request config
+        return request;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(response => {
+        console.log(response);
+        // edit request config
+        return response;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();

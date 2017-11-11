cfa0753ca30a3fd5d42b174bb223b6a37e723016
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 23:19:51 +0300

    Routing: Navigating programmatically

--- routing/src/containers/Blog/Posts/Posts.js
+++ routing/src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import axios from '../../../axios';
 import Post from "../../../components/Post/Post";
 import './Posts.css'
-import {Link} from "react-router-dom";
 
 class Posts extends Component {
     state = {
@@ -31,7 +30,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.props.history.push('/' + id);
     };
 
     render() {
@@ -39,15 +38,11 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link
+                    <Post
                         key={post.id}
-                        to={'/' + post.id}>
-                        <Post
-
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
                 );
             });
         }

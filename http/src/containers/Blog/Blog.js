3abf1782928e4ae01dd015ecef23ec5bb601a1db
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-11 14:02:56 +0300

    Http: Handling Error

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     };
 
     componentDidMount() {
@@ -24,6 +25,9 @@ class Blog extends Component {
 
                 this.setState({posts: updatedPosts});
                 //console.log(response);
+            })
+            .catch(error => {
+                this.setState({error: true});
             });
     }
 
@@ -34,13 +38,16 @@ class Blog extends Component {
     };
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-            />;
-        });
+        let posts = <p style={{textAlign: 'center'}}> Something Wrong </p>;
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />;
+            });
+        }
         return (
             <div>
                 <section className="Posts">

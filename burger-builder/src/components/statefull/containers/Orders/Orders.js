336f9e266696adcdda132f33277a114c502cd375
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-12 23:00:59 +0300

    Burger-builder: Fetching Orders

--- burger-builder/src/components/statefull/containers/Orders/Orders.js
+++ burger-builder/src/components/statefull/containers/Orders/Orders.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Order from "../../../stateless/dummy/Order/Order";
+import baseClient from '../../../../client-base'
+import withErrorHandler from "../../../stateless/hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        baseClient.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        id: key,
+                        ...response.data[key],
+                    });
+                }
+
+                this.setState({
+                    orders: fetchedOrders,
+                    loading: false
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({
+                    loading: false
+                });
+            });
+    }
+
+
     render() {
         return (
             <div>
-                <Order />
+                <Order/>
             </div>
         );
     }
@@ -14,4 +46,4 @@ class Orders extends Component {
 
 Orders.propTypes = {};
 
-export default Orders;
+export default withErrorHandler(Orders, baseClient);

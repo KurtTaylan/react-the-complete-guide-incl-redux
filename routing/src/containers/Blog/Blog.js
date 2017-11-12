b01ef9e96ab626f5ef6ffa1ea9b17a53d9efbd52
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-12 13:49:40 +0300

    Routing: Woring with guards by conditionally render route.

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -5,6 +5,10 @@ import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render() {
         return (
             <div className="Blog">
@@ -30,7 +34,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost}/>
+                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     <Redirect from="/" to="/posts"/>
                 </Switch>

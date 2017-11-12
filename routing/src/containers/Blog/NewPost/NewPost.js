04ed4cb6295075c6756ff8024919962eb2526a29
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-12 13:21:13 +0300

    Routing: Conditional redirect.

--- routing/src/containers/Blog/NewPost/NewPost.js
+++ routing/src/containers/Blog/NewPost/NewPost.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 import './NewPost.css';
+import {Redirect} from "react-router-dom";
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     postDataHandler = () => {
@@ -19,17 +21,27 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
+                this.setState({
+                    submitted: true
+                })
             });
     }
 
-    render () {
+    render() {
+        let redirect = null;
+        if (this.state.submitted) {
+            redirect = <Redirect to="/posts"/>
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title}
+                       onChange={(event) => this.setState({title: event.target.value})}/>
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content}
+                          onChange={(event) => this.setState({content: event.target.value})}/>
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>

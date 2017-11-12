63f0b6e744f28c7a584839abe586799bca3a5164
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-12 14:48:13 +0300

    Routing: Lazy loading module by async component hoc.

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -2,11 +2,16 @@ import React, {Component} from 'react';
 import {NavLink, Redirect, Route, Switch} from 'react-router-dom';
 import './Blog.css';
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+//import NewPost from "./NewPost/NewPost";
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render() {
@@ -34,9 +39,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
-                    <Redirect from="/" to="/posts"/>
+                    <Route render={() => <h1>Not Found</h1>}/>
                 </Switch>
             </div>
         );

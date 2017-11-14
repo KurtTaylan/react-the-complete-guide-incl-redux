94d1c1620d5a6f6c66436d6f0f627ddff998bca0
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-14 23:04:36 +0300

    Redux: Adding Subscription

--- redux/redux-basics.js
+++ redux/redux-basics.js
@@ -29,8 +29,13 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription] ', store.getState())
+});
+
 // Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-// Subscription
+

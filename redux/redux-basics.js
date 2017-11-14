62fc65801cbaad3041ec574ec278370dd744d121
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-14 22:46:09 +0300

    Redux: Setting up Redux and Store

---
+++ redux/redux-basics.js
@@ -0,0 +1,21 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+};
+
+// Reducer
+const rootReducer = (oldState = initialState, action) => {
+    return oldState;
+};
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+
+// Dispatching Action
+
+
+// Subscription

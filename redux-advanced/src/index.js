f1805b0946ec68ff21abdd1517b5882e963f4951
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-16 22:15:34 +0300

    Redux: Using the redux devtools

---
+++ redux-advanced/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+});
+
+const logger = store => next => action => {
+    console.log("Middleware dispatching")
+    const result = next(action);
+    return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(logger)
+));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+registerServiceWorker();

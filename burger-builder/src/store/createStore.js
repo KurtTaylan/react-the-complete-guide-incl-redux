3c6609490797f6fdc309249b38f4640962d2c14b
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-17 21:45:02 +0300

    Burger-builder: Executing Asynchronous code

--- burger-builder/src/store/createStore.js
+++ burger-builder/src/store/createStore.js
@@ -1,4 +1,5 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import burgerBuilderReducer from './reducer/burgerBuilderReducer';
 
 const logger = store => next => action => {
@@ -6,15 +7,16 @@ const logger = store => next => action => {
     let result = next(action);
     console.log('[Middleware] Next State, ', store.getState());
     return result;
-}
+};
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer
 });
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(logger)));
+    composeEnhancers(applyMiddleware(logger, thunk)));
 
 export default store;

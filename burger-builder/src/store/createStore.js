f463de84e355ed0c2b3cd5f2f00cab332055f722
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-30 17:03:52 +0300

    Burger-builder: bugfix, using env variables disable redux debug mod on prod mode

--- burger-builder/src/store/createStore.js
+++ burger-builder/src/store/createStore.js
@@ -5,15 +5,15 @@ import orderReducer from "./reducer/orderReducer";
 import authReducer from "./reducer/authReducer";
 
 const rootReducer = combineReducers({
-    burgerBuilder: burgerBuilderReducer,
-    order: orderReducer,
-    auth: authReducer
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+  auth: authReducer
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk)));
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)));
 
 export default store;

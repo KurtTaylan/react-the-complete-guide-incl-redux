d168ff12fa54f2ebff6016c85c4aa7c063581492
Taylan Kurt <taylan.kurt@iyzico.com>
2017-11-30 11:00:04 +0300

    Burger-builder: auth, Displaying user specific orders

--- burger-builder/src/components/statefull/containers/Orders/Orders.js
+++ burger-builder/src/components/statefull/containers/Orders/Orders.js
@@ -8,44 +8,45 @@ import Spinner from "../../../stateless/dummy/UI/Spinner/Spinner";
 
 
 class Orders extends Component {
-
-    componentDidMount() {
-        this.props.onFetchOrders(this.props.token);
-    }
-
-    render() {
-        let orders = <Spinner/>;
-        if (!this.props.loading) {
-            orders = (
-                <div>
-                    {this.props.orders.map(order => {
-                        return (<Order
-                                key={order.id}
-                                ingredients={order.ingredients}
-                                totalPrice={+order.totalPrice}/>
-                        );
-                    })}
-                </div>
+  
+  componentDidMount() {
+    this.props.onFetchOrders(this.props.token, this.props.userId);
+  }
+  
+  render() {
+    let orders = <Spinner/>;
+    if (!this.props.loading) {
+      orders = (
+        <div>
+          {this.props.orders.map(order => {
+            return (<Order
+                key={order.id}
+                ingredients={order.ingredients}
+                totalPrice={+order.totalPrice}/>
             );
-        }
-        return orders;
+          })}
+        </div>
+      );
     }
+    return orders;
+  }
 }
 
 Orders.propTypes = {};
 
 const mapStateToProps = state => {
-    return {
-        orders: state.order.orders,
-        loading: state.order.loading,
-        token: state.auth.token
-    }
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading,
+    token: state.auth.token,
+    userId: state.auth.userId
+  }
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
-    }
+  return {
+    onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
+  }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, go.baseAPI));
--- burger-builder/src/store/action/orderActions.js
+++ burger-builder/src/store/action/orderActions.js
@@ -2,84 +2,85 @@ import * as actionTypes from './actionTypes'
 import * as go from "../../client";
 
 export const purchaseBurgerInit = () => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_INIT,
-    }
+  return {
+    type: actionTypes.PURCHASE_BURGER_INIT,
+  }
 };
 
 export const purchaseBurgerStart = () => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_START,
-    }
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  }
 };
 
 export const purchaseBurgerSuccess = (id, orderData) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_SUCCESS,
-        orderId: id,
-        orderData: orderData
-    }
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: orderData
+  }
 };
 
 export const purchaseBurgerFailed = (error) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_FAILED,
-        error: error
-    }
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAILED,
+    error: error
+  }
 };
 
 
 export const purchaseBurger = (orderData, token) => {
-    return dispatch => {
-        dispatch(purchaseBurgerStart());
-        go.baseAPI.post('/orders.json?auth=' + token, orderData)
-            .then(response => {
-                dispatch(purchaseBurgerSuccess(response.data.name, response.data))
-            })
-            .catch(error => {
-                dispatch(purchaseBurgerFailed(error))
-            });
-    }
+  return dispatch => {
+    dispatch(purchaseBurgerStart());
+    go.baseAPI.post('/orders.json?auth=' + token, orderData)
+      .then(response => {
+        dispatch(purchaseBurgerSuccess(response.data.name, response.data))
+      })
+      .catch(error => {
+        dispatch(purchaseBurgerFailed(error))
+      });
+  }
 };
 
 export const fetchOrdersStart = () => {
-    return {
-        type: actionTypes.FETCH_ORDERS_START,
-    }
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  }
 };
 
 export const fetchOrdersSuccess = (orders) => {
-    return {
-        type: actionTypes.FETCH_ORDERS_SUCCESS,
-        orders: orders
-    }
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  }
 };
 
 export const fetchOrdersFailed = (error) => {
-    return {
-        type: actionTypes.FETCH_ORDERS_FAILED,
-        error: error
-    }
+  return {
+    type: actionTypes.FETCH_ORDERS_FAILED,
+    error: error
+  }
 };
 
 
-export const fetchOrders = (token) => {
-    return dispatch => {
-        dispatch(fetchOrdersStart());
-        go.baseAPI.get('/orders.json?auth=' + token)
-            .then(response => {
-                const fetchedOrders = [];
-                for (let key in response.data) {
-                    fetchedOrders.push({
-                        id: key,
-                        ...response.data[key],
-                    });
-                }
-                dispatch(fetchOrdersSuccess(fetchedOrders));
-            })
-            .catch(error => {
-                dispatch(fetchOrdersFailed(error))
-            });
-    }
+export const fetchOrders = (token, userId) => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+    go.baseAPI.get('/orders.json' + queryParams)
+      .then(response => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(error => {
+        dispatch(fetchOrdersFailed(error))
+      });
+  }
 };
 

6bedca1cdc809a59384851349aa4582895a96391
Taylan Kurt <taylan.kurt@iyzico.com>
2017-12-01 09:41:54 +0300

    Burger-builder: testing, adding beforeeach to tests

--- burger-builder/src/components/stateless/dummy/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/components/stateless/dummy/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,14 +9,23 @@ configure({adapter: new Adapter()});
 
 describe('<NavigationItems/>', () => {
   
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems/>);
+  });
+  
   it('should render two NavigationItem elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems/>);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
   
   it('should render three NavigationItem elements if authenticated', () => {
-    const wrapper = shallow(<NavigationItems isAuthenticated/>);
+    //wrapper = shallow(<NavigationItems isAuthenticated/>); one alternative
+    wrapper.setProps({isAuthenticated: true});
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
   
+  it('should render logout NavigationItem if authenticated', () => {
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+  });
 });
